d468a26fc891b0c5f9456d7ea3d835e1dfe810c2
Islam Atwa <islamatwa429@gmail.com.com>
2025-03-23 06:46:17 +0200

    - Implemented functionality to send user messages and display them in chat interface.
    - Created a function to generate a bot response using Google API for content generation.
    
    - Added logic to display bot responses after a brief delay.
    
    - Integrated API call to fetch bot responses and update the chat interface accordingly.
    
    - Handled message input and added validation to ensure empty messages aren't sent.
    
    - Updated DOM to append user and bot messages in the chat window.
    
    - Refined chat UI to show messages with appropriate styling for user and bot messages.

---
+++ script.js
@@ -0,0 +1,81 @@
+
+const sendChatBtn = document.querySelector('.chat-footer button');
+const chatInput = document.querySelector('.chat-footer textarea');
+const chatMessages = document.querySelector('.chat-body');
+const dots = document.querySelector('.dots');
+let userMessage; // لتخزين الرسالة التي يكتبها المستخدم
+
+
+// Google Generate Response
+const API_KEY = "AIzaSyBSo2hOo8FamGVkzLJeVyrmHwxQ8gkJYJQ";
+const genrateResponse = (botResponse)=>{
+    const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
+
+    const messageElement = botResponse.querySelector('p');
+
+    const requestOptions = {
+        method: 'POST',
+        headers:{
+            "Content-Type": "application/json",
+            // "Authorization": 'Bearer ${API_KEY}'
+        },
+        body: JSON.stringify({
+            contents: [{ parts: [{ text: userMessage }] }]
+        })
+        
+    }
+        fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
+
+            messageElement.textContent = data.candidates[0].content.parts[0].text;
+            console.log(data);
+        }).catch((error) => {
+            console.log(error);
+        })
+    
+}
+
+
+
+
+
+const createChat = (message, className)=>{
+
+    // إنشاء عنصر div لعرض الرسالة
+    const chatDiv = document.createElement('div');
+    chatDiv.classList.add('chat-message', className);
+    let chatContent;
+    if (className === "user-message") {
+        // إذا كانت الرسالة من المستخدم
+        chatContent = `<div class="message-text usermsg-text"><p>${message}</p></div>`;
+    } else {
+        // إذا كانت الرسالة من الروبوت
+        chatContent = `<img class="botmsg-icon" src="/assets/chatbot.png" alt="">
+        <div class="message-text botmsg-text"><p>${message}</p></div>`;
+    }    
+
+    chatDiv.innerHTML = chatContent;
+    return chatDiv;
+}
+
+
+// عند الضغط على زر إرسال الرسالة
+const handleChat  = () => {
+    userMessage = chatInput.value.trim();
+    if(!userMessage)return;
+    
+    // إضافة رسالة المستخدم إلى الشاشة
+    chatMessages.appendChild(createChat(userMessage, "user-message"));
+    
+    setTimeout(()=>{
+        
+        // إضافة رسالة الروبوت إلى الشاشة
+        const botResponse = createChat("..", "bot-message");
+        chatMessages.appendChild(botResponse);
+        genrateResponse(botResponse);
+
+    },500);
+    // dots.style.display = "block";
+    chatInput.value = "";
+}
+
+sendChatBtn.addEventListener('click', handleChat);
